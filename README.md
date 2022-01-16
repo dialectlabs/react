9e7180090e4f9b0a920fbdf521fce1f7f5169643
cbosborn <chris.osborn@gmail.com>
2022-01-16 16:22:56 -0500

    minor

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dialect
+
+1. import -> export in index.ts
+2. export default in Bell/index.ts
+3. or the .next cache
+
+yarn dev uses the cache, because it's development. 100% sure -> yarn build -> yarn start (check nextjs docs for whatever runs production)
+
--- components/Bell/index.tsx
+++ components/Bell/index.tsx
@@ -3,17 +3,5 @@ import { BellIcon } from '@heroicons/react/outline';
 
 
 export function Bell(): JSX.Element {
-  return <BellIcon className='h-5 w-5' />;
+  return <BellIcon />;
 }
-
-// export const aThing = (): unknown => {
-//   return {thisThing: 'thatThing'};
-// };
-
-/*
-1. import -> export in index.ts
-2. export default in Bell/index.ts
-3. or the .next cache
-
-yarn dev uses the cache, because it's development. 100% sure -> yarn build -> yarn start (check nextjs docs for whatever runs production)
-*/
