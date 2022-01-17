f4385faaf02f683f43dfd092028bd4c894bdeaab
cbosborn <chris.osborn@gmail.com>
2022-01-17 00:22:40 -0500

    Tweaking styles etc.

--- components/Bell/index.tsx
+++ components/Bell/index.tsx
@@ -3,18 +3,21 @@ import { BellIcon } from '@heroicons/react/outline';
 import NotificationCenter from '../NotificationCenter';
 
 export function Bell(): JSX.Element {
-  const [open, setOpen] = useState(true);
+  const [open, setOpen] = useState(false);
   useEffect(() => {
     console.log('open', open);
   }, [open]);
   return (
     <div className='flex flex-col items-end'>
-      <button onClick={() => setOpen(!open)}>
+      <button
+      className='flex items-center justify-center w-9 h-9 rounded-full bg-gray-200'
+        onClick={() => setOpen(!open)}
+      >
         <BellIcon
-          className='p-2 h-9 w-9 bg-gray-200 text-gray-700 rounded-full' />
+          className='w-5 h-5 text-gray-700 rounded-full' />
       </button>
       {open && (
-        <div className='absolute top-14 w-1/3 h-1/2'>
+        <div className='absolute top-30 w-1/3 h-96'>
           <NotificationCenter />
         </div>
       )}
