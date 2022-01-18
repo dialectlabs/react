3d6c93d0dd706b61e5d3333c6bfc581b99820ff6
cbosborn <chris.osborn@gmail.com>
2022-01-17 20:21:47 -0500

    Seems to be correctly using WalletContextProvider, ApicontextProvider, and setting a new WebWallet.

--- components/Bell/index.tsx
+++ components/Bell/index.tsx
@@ -2,13 +2,24 @@ import React, { useEffect, useState } from 'react';
 import { BellIcon } from '@heroicons/react/outline';
 import NotificationCenter from '../NotificationCenter';
 import { AnchorWallet } from '@solana/wallet-adapter-react';
+import { useWallet, ApiContextProvider, WalletContextProvider } from '@dialectlabs/web3';
 
 type PropTypes = {
   wallet: AnchorWallet | undefined;
 }
 
-export function Bell(props: PropTypes): JSX.Element {
+function WrappedBell(props: PropTypes): JSX.Element {
   const [open, setOpen] = useState(false);
+  const { onWebConnect, onWebDisconnect, webWallet } = useWallet();
+  console.log('wallet', props.wallet);
+  console.log('webWallet', webWallet);
+  useEffect(() => {
+    if (props.wallet) {
+      onWebConnect(props.wallet);
+    } else {
+      onWebDisconnect();
+    }
+  }, [props.wallet]);
   return (
     <div className='flex flex-col items-end'>
       <button
@@ -24,5 +35,15 @@ export function Bell(props: PropTypes): JSX.Element {
         </div>
       )}
     </div>
+  );
+}
+
+export function Bell(props: PropTypes): JSX.Element {
+  return (
+    <WalletContextProvider>
+      <ApiContextProvider>
+        <WrappedBell {...props} />
+      </ApiContextProvider>
+    </WalletContextProvider>
   );
 }
