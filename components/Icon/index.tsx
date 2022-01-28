50fef05b564e7fec36382f73c785a8198139fa9b
Kirill Chernakov <hi@kirys.ru>
2022-01-28 18:47:15 +0300

    feat: add icons

---
+++ components/Icon/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Gear from './assets/gear.svg';
+
+// function Icon(params:type) {
+//     return
+// }
+export function GearIcon(): JSX.Element {
+  return <Gear />;
+}
--- next.config.js
+++ next.config.js
@@ -29,6 +29,11 @@ module.exports = withTM({
       os: false,
       crypto: false,
     };
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
     return config;
   },
 });
