7e1403d2e6bc1b57bdd2a03b364b7add4a343e7a
Filipp <filipp.sher@gmail.com>
2022-01-29 14:50:01 +0300

    refactor: adjust eslint config, adjust packages dependencies

--- packages/dialect-react-ui/components/Bell/index.tsx
+++ packages/dialect-react-ui/components/Bell/index.tsx
@@ -4,12 +4,16 @@ import { BellIcon } from '@heroicons/react/outline';
 import NotificationCenter from '../NotificationCenter';
 import { AnchorWallet } from '@solana/wallet-adapter-react';
 import { Wallet } from '@solana/wallet-adapter-wallets';
-import { useWallet, ApiContextProvider, WalletContextProvider } from '@dialectlabs/web3';
+import {
+  useWallet,
+  ApiContextProvider,
+  WalletContextProvider,
+} from '@dialectlabs/web3';
 
 type PropTypes = {
   wallet: AnchorWallet | Wallet | undefined;
   publicKey: anchor.web3.PublicKey;
-}
+};
 
 function WrappedBell(props: PropTypes): JSX.Element {
   const [open, setOpen] = useState(false);
@@ -22,16 +26,15 @@ function WrappedBell(props: PropTypes): JSX.Element {
     }
   }, [props.wallet]);
   return (
-    <div className='flex flex-col items-end'>
+    <div className="flex flex-col items-end">
       <button
-      className='bg-th-bkg-4 flex items-center justify-center rounded-full w-8 h-8 text-th-fgd-1 focus:outline-none hover:text-th-primary'
+        className="bg-th-bkg-4 flex items-center justify-center rounded-full w-8 h-8 text-th-fgd-1 focus:outline-none hover:text-th-primary"
         onClick={() => setOpen(!open)}
       >
-        <BellIcon
-          className='w-4 h-4 rounded-full' />
+        <BellIcon className="w-4 h-4 rounded-full" />
       </button>
       {open && (
-        <div className='z-50 absolute top-14 w-96 h-96'>
+        <div className="z-50 absolute top-14 w-96 h-96">
           <NotificationCenter {...props} />
         </div>
       )}
