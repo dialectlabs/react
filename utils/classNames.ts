782414245d92c75d9281bf9990a135c4db91699d
Kirill Chernakov <hi@kirys.ru>
2022-01-31 15:50:44 +0300

    chore: fix unnamed function error

--- utils/classNames.ts
+++ utils/classNames.ts
@@ -2,9 +2,9 @@
  * Combines CSS class names into `class` attribute string
  * @param  {...any} classNames
  */
-export default (...classNames) => {
+export default function classNames(...classNames): string {
   return classNames
     .filter(Boolean)
     .map((item) => (Array.isArray(item) ? item.join(' ') : item))
     .join(' ');
-};
+}
