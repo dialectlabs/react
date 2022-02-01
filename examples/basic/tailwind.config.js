8646e352455d46b40d0abf942106286d871d06ec
Kirill Chernakov <hi@kirys.ru>
2022-02-01 19:49:10 +0300

    feat: tailwind for local dev

--- examples/basic/tailwind.config.js
+++ examples/basic/tailwind.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  mode: 'jit',
   purge: [
     './{components,pages}/**/*.{js,ts,jsx,tsx}',
     '../../node_modules/@dialectlabs/react-ui/**/*.{js,ts,jsx,tsx}', // In development taking into account workspace files too
+    '../../packages/dialect-react-ui/**/*.{js,ts,jsx,tsx}', // In development taking into account workspace files too
   ],
   theme: {
     fontFamily: {
