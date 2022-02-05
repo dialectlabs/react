e8b08cc3875386003d556ebb1442c91b17edfd1d
Kirill Chernakov <hi@kirys.ru>
2022-02-05 18:13:22 +0300

    feat: make inter a default font

--- examples/dialect.to/pages/_document.jsx
+++ examples/dialect.to/pages/_document.jsx
@@ -15,10 +15,10 @@ class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin={'true'}
+            crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Crimson+Text&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
             rel="stylesheet"
           />
         </Head>
