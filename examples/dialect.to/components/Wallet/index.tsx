0f6dd41fa529717769311d000ea422d977997682
Kirill Chernakov <hi@kirys.ru>
2022-02-05 17:32:05 +0300

    feat: customize wallet button (except text)

--- examples/dialect.to/components/Wallet/index.tsx
+++ examples/dialect.to/components/Wallet/index.tsx
@@ -4,7 +4,7 @@ import {
   WalletMultiButton,
 } from '@solana/wallet-adapter-react-ui';
 // Default styles that can be overridden by your app
-require('@solana/wallet-adapter-react-ui/styles.css');
+import '@solana/wallet-adapter-react-ui/styles.css';
 import { PhantomIcon } from '../Icon';
 
 export const Wallet = () => {
@@ -12,7 +12,7 @@ export const Wallet = () => {
     <>
       <WalletModalProvider>
         <WalletMultiButton
-          className="flex items-center transition ease-in-out duration-300 uppercase text-white text-lg border border-white rounded-full py-2 px-6 font-semibold hover:text-black hover:bg-white bg-transparent"
+          className="dialect-wallet-button"
           startIcon={<PhantomIcon />}
         />
       </WalletModalProvider>
