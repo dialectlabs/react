f169dfc33d571cc405147526901a36f6aeb8f13a
Kirill Chernakov <hi@kirys.ru>
2022-02-08 20:11:43 +0300

    fix: update tailwind config for jet example to take changes into account

--- examples/jet/tailwind.config.js
+++ examples/jet/tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: [
     './{components,pages}/**/*.{js,ts,jsx,tsx}',
     '../../node_modules/@dialectlabs/react-ui/**/*.{js,ts,jsx,tsx}', // In development taking into account workspace files too
-    '../../packages/dialect-react-ui/**/*.{js,ts,jsx,tsx}', // In development taking into account workspace files too
+    '../../packages/dialect-react-ui-jet/**/*.{js,ts,jsx,tsx}', // In development taking into account workspace files too
   ],
   theme: {
     fontFamily: {
