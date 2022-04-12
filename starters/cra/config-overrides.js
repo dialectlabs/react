a321e2a79a24a196bced4e12fa6a1a6e452d6d62
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-04-12 15:28:26 +0300

    fix formatting

--- starters/cra/config-overrides.js
+++ starters/cra/config-overrides.js
@@ -1,15 +1,13 @@
-const {ProvidePlugin} = require('webpack');
-
-module.exports = function (config, env) {
-    return {
-        ...config,
-        resolve: {
-            ...config.resolve,
-            fallback: {
-                crypto: false,
-                stream: false
-            },
-        },
-        ignoreWarnings: [/Failed to parse source map/],
-    };
+module.exports = function (config) {
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      fallback: {
+        crypto: false,
+        stream: false,
+      },
+    },
+    ignoreWarnings: [/Failed to parse source map/],
+  };
 };
