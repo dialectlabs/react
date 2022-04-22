4e564060b5fe718a7d99bea5cbbd7e7c3d4e72fc
Filipp Sher <filipp.sher@gmail.com>
2022-04-23 00:13:18 +0400

    Chore/update exports (#88)
    * chore: remove wildcard imports
    
    * chore: cleanup Changelog, update imports for react-ui, remove Footer, remove logo
    
    * chore: add CHANGELOG entries

--- packages/dialect-react-ui/components/common/index.ts
+++ packages/dialect-react-ui/components/common/index.ts
@@ -1,4 +1,14 @@
-export * from './primitives';
+export {
+  Button,
+  BigButton,
+  Divider,
+  Centered,
+  Loader,
+  Toggle,
+  useBalance,
+  ValueRow,
+  Accordion,
+} from './primitives';
 
 export { default as NetworkBadge } from './NetworkBadge';
 export { default as LinkifiedText } from './LinkifiedText';
