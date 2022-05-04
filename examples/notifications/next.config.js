730a81297e458c5286986ace854dbfd70ee21702
Kirill Chernakov <hi@kirys.ru>
2022-05-04 22:38:19 +0400

    fix: remove proxy because cors is enabled now

--- examples/notifications/next.config.js
+++ examples/notifications/next.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://localhost:8080/:path*', // Proxy to Backend
-      },
-    ];
-  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.svg$/i,
