5c237903b05dc397df388a93408cc795c25730ec
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-06-29 02:13:10 +0300

    update chat exxample

--- examples/chat/pages/index.tsx
+++ examples/chat/pages/index.tsx
@@ -1,16 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 // TODO: retire this example?
 
 import {
   ChatButton,
+  Config,
+  defaultVariables,
+  DialectContextProvider,
+  DialectThemeProvider,
   DialectUiManagementProvider,
   IncomingThemeVariables,
+  Backend,
+  DialectWalletAdapter,
 } from '@dialectlabs/react-ui';
-import { WalletContext, Wallet as WalletButton } from '../components/Wallet';
-import { useWallet } from '@solana/wallet-adapter-react';
-import { defaultVariables } from '@dialectlabs/react-ui';
+import { useWallet, WalletContextState } from '@solana/wallet-adapter-react';
 import Head from 'next/head';
+import { Wallet as WalletButton } from '../components/Wallet';
 // pink: #B852DC
 // teal: #59C29D
 // dark: #353535
@@ -47,8 +52,73 @@ export const themeVariables: IncomingThemeVariables = {
 type ThemeType = 'light' | 'dark' | undefined;
 
 function AuthedHome() {
-  // const wallet = useAnchorWallet();
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <div className="flex flex-col h-screen bg-white dark:bg-black">
+        <div className="flex flex-row justify-end p-2 items-center space-x-2">
+          <ChatButton dialectId="dialect-chat" />
+          <WalletButton />
+        </div>
+        <div className="h-full text-2xl flex flex-col justify-center items-center">
+          <code className="text-center text-neutral-400 dark:text-neutral-600 text-sm mb-2">
+            @dialectlabs/react
+          </code>
+          <div>
+            <code className="shrink text-center text-transparent bg-clip-text bg-gradient-to-r from-[#B852DC] to-[#59C29D]">
+              examples/chat
+            </code>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+const walletToDialectWallet = (
+  wallet: WalletContextState
+): DialectWalletAdapter => ({
+  publicKey: wallet.publicKey!,
+  connected:
+    wallet.connected &&
+    !wallet.connecting &&
+    !wallet.disconnecting &&
+    Boolean(wallet.publicKey),
+  signMessage: wallet.signMessage,
+  signTransaction: wallet.signTransaction,
+  signAllTransactions: wallet.signAllTransactions,
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  diffieHellman: wallet.wallet?.adapter?._wallet?.diffieHellman
+    ? async (pubKey) => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        return wallet.wallet?.adapter?._wallet?.diffieHellman(pubKey);
+      }
+    : undefined,
+});
+
+export default function Home(): JSX.Element {
   const wallet = useWallet();
+  const [dialectWalletAdapter, setDialectWalletAdapter] =
+    useState<DialectWalletAdapter>(() => walletToDialectWallet(wallet));
+
+  useEffect(() => {
+    setDialectWalletAdapter(walletToDialectWallet(wallet));
+  }, [wallet]);
+
   const [theme, setTheme] = useState<ThemeType>('dark');
 
   useEffect(() => {
@@ -68,53 +138,24 @@ function AuthedHome() {
       });
   }, []);
 
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <div className="flex flex-col h-screen bg-white dark:bg-black">
-        <div className="flex flex-row justify-end p-2 items-center space-x-2">
-          <ChatButton
-            dialectId="dialect-chat"
-            wallet={wallet}
-            network={'localnet'}
-            theme={theme}
-            variables={themeVariables}
-          />
-          <WalletButton />
-        </div>
-        <div className="h-full text-2xl flex flex-col justify-center items-center">
-          <code className="text-center text-neutral-400 dark:text-neutral-600 text-sm mb-2">
-            @dialectlabs/react
-          </code>
-          <div>
-            <code className="shrink text-center text-transparent bg-clip-text bg-gradient-to-r from-[#B852DC] to-[#59C29D]">
-              examples/chat
-            </code>
-          </div>
-        </div>
-      </div>
-    </>
+  const dialectConfig = useMemo(
+    (): Config => ({
+      backends: [Backend.DialectCloud, Backend.Solana],
+      environment: 'local-development',
+    }),
+    []
   );
-}
 
-export default function Home(): JSX.Element {
   return (
-    <WalletContext>
+    <DialectContextProvider
+      config={dialectConfig}
+      wallet={dialectWalletAdapter}
+    >
       <DialectUiManagementProvider>
-        <AuthedHome />
+        <DialectThemeProvider theme={theme} variables={themeVariables}>
+          <AuthedHome />
+        </DialectThemeProvider>
       </DialectUiManagementProvider>
-    </WalletContext>
+    </DialectContextProvider>
   );
 }
