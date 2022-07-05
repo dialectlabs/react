b9514e38a95fd2fb06f1f3fdaf933f8b35eb708a
Kirill Chernakov <hi@kirys.ru>
2022-07-05 17:42:08 +0400

    feature: add `useDapp` and `useDappAddresses` hooks to the `react-sdk`

---
+++ packages/dialect-react-sdk/hooks/useDapp.ts
@@ -0,0 +1,42 @@
+import type {
+  Dapp,
+  DialectSdkError,
+  DialectWalletAdapter,
+} from '@dialectlabs/sdk';
+import useSWR from 'swr';
+import { EMPTY_OBJ } from '../utils';
+import useDialectSdk from './useDialectSdk';
+
+const DAPP_CACHE_KEY = (wallet: DialectWalletAdapter) =>
+  'DAPPS_' + wallet?.publicKey?.toBase58();
+
+interface UseDappValue {
+  dapp: Dapp | null;
+  isFetching: boolean;
+  errorFetching: DialectSdkError | null;
+}
+
+interface UseDappParams {
+  refreshInterval?: number;
+}
+
+// FIXME: extend
+function useDapp({ refreshInterval }: UseDappParams = EMPTY_OBJ): UseDappValue {
+  const { dapps } = useDialectSdk();
+  const {
+    info: { wallet },
+  } = useDialectSdk();
+  const { data: dapp = null, error } = useSWR(
+    DAPP_CACHE_KEY(wallet),
+    () => dapps.find(),
+    { refreshInterval, refreshWhenOffline: true }
+  );
+
+  return {
+    dapp,
+    isFetching: !error && !dapp,
+    errorFetching: error,
+  };
+}
+
+export default useDapp;
---
+++ packages/dialect-react-sdk/hooks/useDappAddresses.ts
@@ -0,0 +1,55 @@
+import type { Dapp, DappAddress, DialectSdkError } from '@dialectlabs/sdk';
+import { useEffect } from 'react';
+import useSWR from 'swr';
+import { EMPTY_ARR, EMPTY_OBJ } from '../utils';
+import useDapp from './useDapp';
+
+const DAPP_ADDRESSES_CACHE_KEY = (dapp: Dapp | null) =>
+  'DAPP_ADDRESSES_' + dapp?.publicKey;
+
+interface UseDappAddressesValue {
+  addresses: DappAddress[];
+  isFetching: boolean;
+  errorFetching: DialectSdkError | null;
+}
+
+interface UseDappAddressesParams {
+  refreshInterval?: number;
+}
+
+function useDappAddresses({
+  refreshInterval,
+}: UseDappAddressesParams = EMPTY_OBJ): UseDappAddressesValue {
+  const { dapp } = useDapp();
+  const dappAddressesApi = dapp?.dappAddresses;
+
+  const {
+    data: addresses = EMPTY_ARR,
+    isValidating: isFetching,
+    error = null,
+    mutate,
+  } = useSWR(
+    DAPP_ADDRESSES_CACHE_KEY(dapp),
+    dappAddressesApi ? () => dappAddressesApi.findAll() : null,
+    {
+      refreshInterval,
+      refreshWhenOffline: true,
+    }
+  );
+
+  useEffect(
+    function invalidateAddresses() {
+      mutate();
+    },
+    [mutate, dappAddressesApi]
+  );
+
+  return {
+    addresses,
+    isFetching,
+    // isFetching: Boolean(dapp) && !error && !addresses,
+    errorFetching: error,
+  };
+}
+
+export default useDappAddresses;
