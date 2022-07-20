fead6188f8e271d366365e0ada7ca9fd85ca93c6
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2022-07-20 16:17:36 +0300

    minor cleanup

--- packages/dialect-react-sdk/hooks/index.ts
+++ packages/dialect-react-sdk/hooks/index.ts
@@ -3,7 +3,6 @@ export { default as useDialectDapp } from './useDialectDapp';
 export { default as useThreads } from './useThreads';
 export { default as useThread } from './useThread';
 export { default as useThreadMessages } from './useThreadMessages';
-export { default as useDialectCloudApi } from './useDialectCloudApi';
 export { default as useDialectWallet } from './useDialectWallet';
 export { default as useDialectConnectionInfo } from './useDialectConnectionInfo';
 export { default as useUnreadMessages } from './useUnreadMessages';
