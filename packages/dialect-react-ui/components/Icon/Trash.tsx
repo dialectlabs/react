e5daf76b41b17ff851f9b5d939dde57a4dccd9de
cbosborn <chris.osborn@gmail.com>
2022-07-31 12:37:19 -0400

    Reduce size of trash can.

--- packages/dialect-react-ui/components/Icon/Trash.tsx
+++ packages/dialect-react-ui/components/Icon/Trash.tsx
@@ -2,10 +2,12 @@ import type { SVGProps } from 'react';
 
 const SvgTrash = (props: SVGProps<SVGSVGElement>) => (
   <svg
-    width={24}
-    height={24}
+    width={16}
+    height={16}
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio='xMidYMid'
+    viewBox="0 0 24 24"
     {...props}
   >
     <path
