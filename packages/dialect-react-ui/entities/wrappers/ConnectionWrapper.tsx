a1103a0017c5e2ee81d96a86b7d38761ef45422e
Filipp Sher <filipp.sher@gmail.com>
2022-08-26 14:53:24 +0400

    chore: add constant for connectivity polling

--- packages/dialect-react-ui/entities/wrappers/ConnectionWrapper.tsx
+++ packages/dialect-react-ui/entities/wrappers/ConnectionWrapper.tsx
@@ -3,7 +3,6 @@ import NoConnectionError from '../errors/ui/NoConnectionError';
 import type { ReactNode } from 'react';
 
 // Only renders children if connected to successfully some backend
-
 interface ConnectionWrapperProps {
   header?: JSX.Element | null;
   pollingInterval?: number;
@@ -12,11 +11,13 @@ interface ConnectionWrapperProps {
 
 // FIXME: trigger some hook to check connection
 
+const DEFAULT_CONNECTIVITY_POLLING_INTERVAL = 5000;
+
 export default function ConnectionWrapper({
   header,
   // fallback to a separate value, since this is more about the connection, rather than fetching functionality
   // potentially can be a separate setting completely, TBD.
-  pollingInterval = 5000,
+  pollingInterval = DEFAULT_CONNECTIVITY_POLLING_INTERVAL,
   children,
 }: ConnectionWrapperProps): JSX.Element {
   // TODO: take into account offline
