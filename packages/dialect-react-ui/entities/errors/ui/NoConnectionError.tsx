67764fe46b70ea8d24ab878fd91008d5314d7a97
Kirill Chernakov <hi@kirys.ru>
2022-09-02 17:10:03 +0300

    refactor: allow to pass message to `<NoConnectionError/>`

--- packages/dialect-react-ui/entities/errors/ui/NoConnectionError.tsx
+++ packages/dialect-react-ui/entities/errors/ui/NoConnectionError.tsx
@@ -1,15 +1,17 @@
 import { Centered } from '../../../components/common';
 import { useTheme } from '../../../components/common/providers/DialectThemeProvider';
 
-const NoConnectionError = () => {
+interface NoConnectionErrorProps {
+  message?: string | JSX.Element;
+}
+
+const NoConnectionError = ({ message }: NoConnectionErrorProps) => {
   const { icons } = useTheme();
 
   return (
     <Centered>
       <icons.offline className="dt-w-10 dt-mb-6 dt-opacity-60" />
-      <span className="dt-opacity-60">
-        Waiting for connection to Solana blockchain
-      </span>
+      <span className="dt-opacity-60">{message}</span>
     </Centered>
   );
 };
