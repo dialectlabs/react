81a99c462047a353eee7489fb059178f54c5d729
Kirill Chernakov <hi@kirys.ru>
2022-09-09 16:26:28 +0300

    - fix: swap out solscan for solanafm in all account links in react web

--- packages/dialect-react-ui/CHANGELOG.md
+++ packages/dialect-react-ui/CHANGELOG.md
@@ -2,6 +2,8 @@
 
 ## [UNRELEASED]
 
+- fix: swap out solscan for solanafm in all account links in react web
+
 ## [1.0.0-beta.55] - 2022-09-08
 
 - fix: subscribe button causing an infinite sign message loop if the wallet is connected but token is not present
--- packages/dialect-react-ui/utils/getExplorerAddress.ts
+++ packages/dialect-react-ui/utils/getExplorerAddress.ts
@@ -1,13 +1,15 @@
+const RELIEABLE_DEVNET = 'devnet-solana';
+
 export const getExplorerAddress = (
   address: string,
   cluster: string | null | undefined
 ): string => {
-  if (!cluster) {
-    cluster = 'devnet';
+  if (!cluster || cluster === 'devnet') {
+    cluster = RELIEABLE_DEVNET;
   } else if (cluster === 'mainnet') {
     cluster = '';
   }
-  return `https://solscan.io/account/${address}${
+  return `https://solana.fm/address/${address}${
     cluster ? `?cluster=${cluster}` : ''
   }`;
 };
