5d55114d2fcc42ef47d192e41d055a80748f525a
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-04-01 17:06:54 +0300

    add button

---
+++ packages/dialect-react-ui-next/stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Button } from '../ui/core/primitives';
+
+export const Main: StoryObj<typeof Button> = {
+  args: {
+    children: 'Press me',
+    // label: 'Input',
+    // value: undefined,
+    // placeholder: 'Type here...',
+  },
+};
+
+export default {
+  component: Button,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 400 }}>
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof Button>;
---
+++ packages/dialect-react-ui-next/ui/core/primitives/Button.tsx
@@ -0,0 +1,34 @@
+import clsx from 'clsx';
+import { ClassTokens, Icons } from '../../../model';
+
+export interface ButtonProps {
+  children: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+  loading?: boolean;
+}
+
+export const Button = (props: ButtonProps): JSX.Element => {
+  return (
+    <button
+      className={clsx(
+        'dt-button dt-p-2 dt-rounded-lg dt-flex dt-justify-center dt-items-center dt-gap-1.5',
+        ClassTokens.Background.Button.Default,
+        ClassTokens.Background.Button.Hover,
+        ClassTokens.Background.Button.Pressed,
+        ClassTokens.Text.Button.Disabled,
+        ClassTokens.Background.Button.Disabled,
+      )}
+      onClick={props.onClick}
+      disabled={props.disabled || props.loading}
+    >
+      {props.loading ? (
+        <>
+          Loading <Icons.Loader />
+        </>
+      ) : (
+        props.children
+      )}
+    </button>
+  );
+};
