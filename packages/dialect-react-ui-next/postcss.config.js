68d71bed21fb6cb4fbab14761a78666f1cf043fc
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-04-02 15:00:18 +0300

    cleanup index.css with prefixed preflights

--- packages/dialect-react-ui-next/postcss.config.js
+++ packages/dialect-react-ui-next/postcss.config.js
@@ -1,6 +1,22 @@
+let prefixOverrideList = ['html', 'body'];
+
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'postcss-prefix-selector': {
+      prefix: '.dialect',
+      includeFiles: ['index.css'],
+      transform: function (prefix, selector, prefixedSelector) {
+        if (selector === '.dialect') {
+          return selector;
+        }
+        if (prefixOverrideList.includes(selector)) {
+          return prefix;
+        } else {
+          return prefixedSelector;
+        }
+      },
+    },
   },
 };
