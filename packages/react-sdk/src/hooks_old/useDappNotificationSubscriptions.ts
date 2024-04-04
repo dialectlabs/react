4a39e30e58bed11478461266bd6854c7cc45a8b9
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-04-04 16:06:23 +0300

    add old hooks

---
+++ packages/react-sdk/src/hooks_old/useDappNotificationSubscriptions.ts
@@ -0,0 +1,42 @@
+import type {
+  DappNotificationSubscription,
+  DialectSdkError,
+} from '@dialectlabs/sdk';
+import useSWR from 'swr';
+import { EMPTY_ARR, EMPTY_OBJ } from '../utils';
+import { DAPP_NOTIFICATION_SUBSCRIPTIONS_CACHE_KEY_FN } from './internal/swrCache';
+import useDapp from './useDapp';
+
+interface UseDappNotificationSubscriptionsValue {
+  subscriptions: DappNotificationSubscription[];
+  isFetching: boolean;
+  errorFetching: DialectSdkError | null;
+}
+
+interface UseDappNotificationSubscriptions {
+  refreshInterval?: number;
+}
+
+function useDappNotificationSubscriptions({
+  refreshInterval,
+}: UseDappNotificationSubscriptions = EMPTY_OBJ): UseDappNotificationSubscriptionsValue {
+  const { dapp } = useDapp();
+  const subscriptionsApi = dapp?.notificationSubscriptions;
+
+  const { data: subscriptions, error: errorFetching = null } = useSWR(
+    DAPP_NOTIFICATION_SUBSCRIPTIONS_CACHE_KEY_FN(dapp?.address),
+    subscriptionsApi ? () => subscriptionsApi.findAll() : null,
+    {
+      refreshInterval,
+      refreshWhenOffline: true,
+    }
+  );
+
+  return {
+    subscriptions: subscriptions || EMPTY_ARR,
+    isFetching: Boolean(dapp) && !errorFetching && subscriptions === undefined,
+    errorFetching,
+  };
+}
+
+export default useDappNotificationSubscriptions;
---
+++ packages/react-sdk/src/hooks_old/useThreadMessages.ts
@@ -0,0 +1,230 @@
+import {
+  DialectSdkError,
+  SendMessageCommand as DialectSdkSendMessageCommand,
+  Thread,
+  ThreadId,
+  ThreadMessage as SdkThreadMessage,
+} from '@dialectlabs/sdk';
+import { nanoid } from 'nanoid';
+import { useCallback, useMemo, useState } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
+import { LocalMessages } from '../context/DialectContext/LocalMessages';
+import type { LocalThreadMessage } from '../types';
+import { EMPTY_ARR, isOffChain, isOnChain } from '../utils';
+import {
+  CACHE_KEY_MESSAGES_FN,
+  CACHE_KEY_THREADS,
+  CACHE_KEY_THREAD_SUMMARY_FN,
+} from './internal/swrCache';
+import useThread from './useThread';
+
+interface SendMessageCommand extends DialectSdkSendMessageCommand {
+  id?: string;
+}
+
+interface CancelMessageCommand {
+  id: string;
+}
+
+interface UseThreadMessagesParams {
+  id: ThreadId;
+  refreshInterval?: number;
+}
+
+interface UseThreadMessagesValue {
+  // sdk
+  messages: LocalThreadMessage[];
+
+  // react-lib
+  send(command: SendMessageCommand): Promise<void>;
+  cancel(cmd: CancelMessageCommand): Promise<void>;
+  markAsRead(): Promise<void>;
+
+  isFetchingMessages: boolean;
+  errorFetchingMessages: DialectSdkError | null;
+  isSendingMessage: boolean;
+  errorSendingMessage: DialectSdkError | null;
+}
+
+const useThreadMessages = ({
+  id,
+  refreshInterval,
+}: UseThreadMessagesParams): UseThreadMessagesValue => {
+  const { thread } = useThread({
+    findParams: {
+      id,
+    },
+  });
+  const threadInternal = thread as Thread | null;
+  const { mutate: globalMutate } = useSWRConfig();
+
+  const [isSendingMessage, setIsSendingMessage] = useState<boolean>(false);
+  const [errorSendingMessage, setErrorSendingMessage] =
+    useState<DialectSdkError | null>(null);
+
+  const { localMessages, putLocalMessage, deleteLocalMessage } =
+    LocalMessages.useContainer();
+
+  const {
+    data: remoteMessages,
+    error: errorFetchingMessages = null,
+    mutate,
+  } = useSWR<SdkThreadMessage[], DialectSdkError>(
+    threadInternal ? CACHE_KEY_MESSAGES_FN(threadInternal.id.toString()) : null,
+    () => threadInternal!.messages(),
+    { refreshInterval, refreshWhenOffline: true }
+  );
+
+  const messages: LocalThreadMessage[] | null = useMemo(() => {
+    let messageArray: SdkThreadMessage[] = [];
+
+    if (!thread) {
+      return null;
+    }
+
+    const localThreadMessages = localMessages[thread.id.toString()];
+
+    const filteredLocalMessages = localThreadMessages?.filter((lm) => {
+      const remoteMessage = remoteMessages?.find(
+        (rm) => rm.deduplicationId === lm.deduplicationId
+      );
+
+      if (remoteMessage) {
+        deleteLocalMessage(thread.id.toString(), lm.deduplicationId);
+      }
+
+      return !remoteMessage;
+    });
+
+    // If both local and remote messages have not been fetched yet return null to keep accurate `isFetching` state
+    if (!filteredLocalMessages && !remoteMessages) {
+      return null;
+    }
+
+    // For backends other than `DialectCloud` we return null if no remote meessages
+    if (isOnChain(thread.type) && !remoteMessages) {
+      return null;
+    }
+
+    // If there are remote messages add them to the `messageArray`
+    if (isOffChain(thread.type) && remoteMessages) {
+      messageArray = messageArray.concat(remoteMessages);
+    }
+
+    // If there are local messages add them to the `messageArray` as well
+    if (isOffChain(thread.type) && filteredLocalMessages) {
+      messageArray = messageArray.concat(filteredLocalMessages);
+    }
+
+    return messageArray
+      .sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime())
+      .map((m) => ({
+        ...m,
+        deduplicationId: m.deduplicationId ?? m.timestamp.getTime().toString(),
+      }));
+  }, [remoteMessages, thread, localMessages, deleteLocalMessage]);
+
+  const onChainSendMessage = useCallback(
+    async (t: Thread, cmd: Omit<SendMessageCommand, 'deduplicationId'>) => {
+      setErrorSendingMessage(null);
+      setIsSendingMessage(true);
+
+      try {
+        await t.send(cmd);
+        mutate();
+        // Mutate threads to update threads sort
+        globalMutate(CACHE_KEY_THREADS);
+      } catch (e) {
+        if (e instanceof DialectSdkError) {
+          setErrorSendingMessage(e);
+        }
+        throw e;
+      } finally {
+        setIsSendingMessage(false);
+      }
+    },
+    [globalMutate, mutate]
+  );
+
+  const offChainSendMessage = useCallback(
+    async (t: Thread, cmd: SendMessageCommand) => {
+      setIsSendingMessage(true);
+      setErrorSendingMessage(null);
+      const threadAddr = t.id.toString();
+      const deduplicationId = cmd.deduplicationId ?? nanoid();
+      const optimisticMessage: LocalThreadMessage = {
+        deduplicationId,
+        text: cmd.text,
+        timestamp: new Date(),
+        author: t.me,
+        isSending: true,
+      };
+      try {
+        putLocalMessage(threadAddr, optimisticMessage);
+        await t.send({ ...cmd, deduplicationId });
+        mutate();
+        // Mutate threads to update threads sort
+        globalMutate(CACHE_KEY_THREADS);
+      } catch (e) {
+        if (e instanceof DialectSdkError) {
+          setErrorSendingMessage(e);
+          putLocalMessage(threadAddr, {
+            ...optimisticMessage,
+            isSending: false,
+            error: e,
+          });
+        }
+        throw e;
+      } finally {
+        setIsSendingMessage(false);
+      }
+    },
+    [globalMutate, mutate, putLocalMessage]
+  );
+
+  const sendMessage = useCallback(
+    async (cmd: SendMessageCommand) => {
+      if (!threadInternal) return;
+
+      const sendMessageFn = isOffChain(threadInternal.type)
+        ? offChainSendMessage
+        : onChainSendMessage;
+
+      await sendMessageFn(threadInternal, cmd);
+    },
+    [threadInternal, offChainSendMessage, onChainSendMessage]
+  );
+
+  const cancelMessage = useCallback(
+    async ({ id }: CancelMessageCommand) => {
+      if (!thread) return;
+      deleteLocalMessage(thread.id.toString(), id);
+    },
+    [thread, deleteLocalMessage]
+  );
+
+  const markAsRead = useCallback(async () => {
+    if (!threadInternal) return;
+    await threadInternal.markAsRead();
+    globalMutate(
+      CACHE_KEY_THREAD_SUMMARY_FN(
+        threadInternal.otherMembers.map((it) => it.address)
+      )
+    );
+  }, [globalMutate, threadInternal]);
+
+  return {
+    messages: messages || EMPTY_ARR,
+    send: sendMessage,
+    cancel: cancelMessage,
+    markAsRead,
+
+    // Do not use `isValidating` since it will produce visual flickering
+    isFetchingMessages: !messages && !errorFetchingMessages,
+    errorFetchingMessages,
+    isSendingMessage,
+    errorSendingMessage,
+  };
+};
+
+export default useThreadMessages;
