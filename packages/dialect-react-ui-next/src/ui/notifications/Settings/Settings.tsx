c2337ec98da445dd526e6064dfd78ca022b663e1
Victoria Prusakova <prsvic@gmail.com>
2024-04-12 20:18:01 +0300

    add settings screen loader

--- packages/dialect-react-ui-next/src/ui/notifications/Settings/Settings.tsx
+++ packages/dialect-react-ui-next/src/ui/notifications/Settings/Settings.tsx
@@ -1,16 +1,39 @@
+import {
+  useDialectContext,
+  useNotificationChannelDappSubscription,
+  useNotificationSubscriptions,
+} from '@dialectlabs/react-sdk';
+import { AddressType } from '@dialectlabs/sdk';
 import clsx from 'clsx';
 import { ClassTokens } from '../../theme';
 import { useExternalProps } from '../internal/ExternalPropsProvider';
 import { AppInfo } from './AppInfo';
 import { Channels } from './Channels';
 import { NotificationTypes } from './NotificationTypes';
+import { SettingsLoading } from './SettingsLoading';
 import { TosAndPrivacy } from './TosAndPrivacy';
 import { TryDialectOperator } from './TryDialectOperator';
 
 export const Settings = () => {
+  const { dappAddress } = useDialectContext();
+
+  const subscription = useNotificationChannelDappSubscription({
+    addressType: AddressType.Wallet,
+    dappAddress,
+  });
+
+  const { isFetching: isFetchingNotificationsSubscriptions } =
+    useNotificationSubscriptions({ dappAddress });
+
+  const isLoading =
+    subscription.isFetchingSubscriptions ||
+    isFetchingNotificationsSubscriptions;
+
   const { channels } = useExternalProps();
 
-  return (
+  return isLoading ? (
+    <SettingsLoading />
+  ) : (
     <div className="dt-flex dt-h-full dt-flex-col">
       <div className="dt-px-4 dt-py-3">
         <Channels channels={channels} />
---
+++ packages/dialect-react-ui-next/src/ui/notifications/Settings/SettingsLoading.tsx
@@ -0,0 +1,21 @@
+import clsx from 'clsx';
+import { ClassTokens, Icons } from '../../theme';
+
+export const SettingsLoading = () => {
+  return (
+    <div className="dt-flex dt-h-full dt-flex-1 dt-flex-col dt-items-center dt-justify-center dt-px-4">
+      <div className={clsx(ClassTokens.Icon.Secondary)}>
+        <Icons.Loader width={24} height={24} />
+      </div>
+      {/*TODO ???*/}
+      {/*<h3*/}
+      {/*  className={clsx(*/}
+      {/*    'dt-pb-2 dt-pt-6 dt-text-h2 dt-font-semibold',*/}
+      {/*    ClassTokens.Text.Primary,*/}
+      {/*  )}*/}
+      {/*>*/}
+      {/*  Loading settings*/}
+      {/*</h3>*/}
+    </div>
+  );
+};
