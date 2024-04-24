3bc66007ecddd242728aac36ea735822be943a49
Nick Zhelezkov <nick.zhelezkov@gmail.com>
2024-04-24 15:50:28 +0300

    remove "try dialect operator"

--- packages/react-ui/src/ui/notifications/Settings/Settings.tsx
+++ packages/react-ui/src/ui/notifications/Settings/Settings.tsx
@@ -12,7 +12,6 @@ import { Channels } from './Channels';
 import { NotificationTypes } from './NotificationTypes';
 import { SettingsLoading } from './SettingsLoading';
 import { TosAndPrivacy } from './TosAndPrivacy';
-import { TryDialectOperator } from './TryDialectOperator';
 
 export const Settings = () => {
   const { dappAddress } = useDialectContext();
@@ -42,9 +41,6 @@ export const Settings = () => {
         <NotificationTypes />
       </div>
       <div className="dt-flex-1" />
-      <div className="dt-px-4 dt-py-4">
-        <TryDialectOperator />
-      </div>
       <div
         className={clsx(
           'dt-flex dt-flex-col dt-gap-2 dt-border-t dt-px-4 dt-py-4',
