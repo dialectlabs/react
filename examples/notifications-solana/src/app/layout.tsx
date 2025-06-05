e6bb18460aa44daafeda5f7e51c3585c7c20fdde
Filipp Sher <filipp.sher@gmail.com>
2025-06-05 17:31:11 +0300

    chore: add missing suspense

--- examples/notifications-solana/src/app/layout.tsx
+++ examples/notifications-solana/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Suspense } from 'react';
 import './globals.css';
 import { Providers } from './providers';
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Suspense fallback={null}>
+          <Providers>{children}</Providers>
+        </Suspense>
       </body>
     </html>
   );
